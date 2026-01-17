847488d292be805420f8aecc5352ae9d28feb178
SIVAPRASAD G <sivaprasad.g@live.com>
2026-01-17 15:36:11 +0530

    initalphase

---
+++ 01 Introduction to JavaScript Programming/greeting.js
@@ -0,0 +1,3 @@
+console.log("Hello, my name is Siva!");
+console.log("I am learning Javascript.");
+console.log("It's fun to code!");
---
+++ 01 Introduction to JavaScript Programming/post.js
@@ -0,0 +1,10 @@
+// Creating a multi-line post about learning JavaScript
+console.log("🎉 Day 1 of Learning JavaScript! 🎉");
+console.log("---------------------------------");
+console.log("Just wrote my first lines of code!");
+console.log("Things I've learned so far:");
+console.log("1. How to use console.log()");
+console.log("2. How to write messages");
+console.log("3. How to create multi-line output");
+console.log("---------------------------------");
+console.log("Can't wait to learn more! 💻");
